0cefecd9e5720af7b9d6dde85bd19530d19e3d7b
nikita <inaevnikita208@gmail.com>
2023-03-31 09:13:00 +1000

    Commit request

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate(double height) {
-        int weight = 94;
+    public double calculate(double height, int weight) {
+        weight = 94;
         double index = height * height;
         double bmi = (double) (weight / index);
 
